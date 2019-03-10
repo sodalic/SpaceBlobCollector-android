5ab1055c87cccfb084fb2da85d937d74636f2793
zagorsky <josh.zagorsky@gmail.com>
2019-03-10 00:29:21 -0500

    Updates app level to 37; version still 2.5.0. This is just because the Google Play Store requires each submission to have a new level code.

--- app/src/main/java/org/beiwe/app/DeviceInfo.java
+++ app/src/main/java/org/beiwe/app/DeviceInfo.java
@@ -69,7 +69,7 @@ public class DeviceInfo {
 	 * 34: app version 2.4.3, Removes "Call My Clinician" and "Call Research Assistant" button
 	 *      for studies that have those set in App Settings. Defaults to showing the buttons.
 	 * 35: app version 2.4.4, Allow Markdown URL links to be clickable
-	 * 36: app version 2.5.0, googlePlayStore build variants no longer ask for SMS/MMS and Call Log
+	 * 37: app version 2.5.0, googlePlayStore build variants no longer ask for SMS/MMS and Call Log
 	 *      permissions because of Google policy changes effective March 9th, 2019.  onnelaLabServer
 	 *      build variants still collect SMS/MMS and Call Log data.  */
 
